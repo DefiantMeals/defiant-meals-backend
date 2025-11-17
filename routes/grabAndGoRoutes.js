75fc1136cd1974bde8b19eee4ce1c58d5898f95f
Nicole Babb <defiantmealsmenu@gmail.com>
2025-11-17 16:12:06 +0700

    Add missing create-checkout-session route

--- routes/grabAndGoRoutes.js
+++ routes/grabAndGoRoutes.js
@@ -4,6 +4,7 @@ const grabAndGoController = require('../controllers/grabAndGoController');
 
 // Public routes
 router.get('/menu', grabAndGoController.getGrabAndGoMenu);
+router.post('/create-checkout-session', grabAndGoController.createCheckoutSession);
 
 // Admin routes (you can add auth middleware later if needed)
 router.get('/orders', grabAndGoController.getAllGrabAndGoOrders);
