467355b9827c0a10e28a45706a3ec413fda2c436
Your Name <your.email@example.com>
2025-12-15 19:44:12 +0700

    Revert "Add servingSize field, contact form endpoint"
    This reverts commit 75076712e09951d8b70d4fde47896c19e737d12c.

--- models/Menu.js
+++ models/Menu.js
@@ -48,7 +48,6 @@ const menuSchema = new mongoose.Schema(
     protein: { type: String, default: '' },
     fats: { type: String, default: '' },
     carbs: { type: String, default: '' },
-    servingSize: { type: String, default: '' },
     flavorOptions: [flavorOptionSchema],
     addonOptions: [addonOptionSchema],
     allowFlavorCustomization: {
